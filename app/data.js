440184de5291d51ebf49b3337abd7af6b4aeab86
[Ethan Jarrell] <[ethan.jarrell@gmail.com]>
2017-06-28 17:55:13 -0400

    my next commit

--- app/data.js
+++ app/data.js
@@ -1,8 +1,34 @@
-module.exports =
-{unguessedLetters : ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",]},
+const fs = require('fs');
+const listOfWords = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 
-{listOfWords:["apples", "bananas", "strawberries", "mangos", "pineapples", "watermelon"]},
 
-{guessedLetters : []},
+  function getRandomWord(myArray){
+    var randomValue = myArray[Math.floor(Math.random() * myArray.length)];
+    return randomValue;
+    }
+    var randomWord = getRandomWord(listOfWords);
 
-{currentWord:[]}
+    function makeWordAnObject(strArr){
+    strArr.split("");
+    str = [];
+    for (var i = 0; i < strArr.length; i++){
+    let c = strArr[i];
+    str.push({"letter": c})};
+     return{str};
+    }
+
+   let newWord = makeWordAnObject(randomWord);
+   console.log(newWord);
+
+//    var findOne = function (letterarray, wordarray) {
+//     return wordarray.some(function (v) {
+//         return letterarray.indexOf(v) >= 0;
+//     });
+// };
+//
+// findOne(guessedLetters, newWord);
+// console.log(findOne);
+
+   module.exports = {
+     newWord: str
+   }
--- app/index.js
+++ app/index.js
@@ -7,6 +7,9 @@ const expressValidator = require('express-Validator');
 const mustacheExpress = require('mustache-express');
 const data = require('./data.js');
 const app = express();
+let guessedLetters = [];
+// const fs = require('fs');
+// const listOfWords = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(expressValidator());
@@ -22,20 +25,23 @@ app.engine('mustache', mustacheExpress());
 
 //function to render guessed letters
 app.get('/', function(req, res){
-  res.render('index', {guessedLetters: data.guessedLetters});
+  res.render('index', {newWord: data.newWord});
+  console.log(guessedLetters);
 });
 
+//Changes to make:::.  Put everything in data.js including the fs and use the code have below but add [i] to the array to select the index value of the word, and then figure out thow to display it in the HTML
 
 //function to pick a random word from the List of Words
-app.get('/', function(req, res){
-  var randomValue = myArray[Math.floor(Math.random() * data.listOfWords.length)];
-  res.render('index', {listOfWords: data.listOfWords})
-});
+// app.get('/', function(req, res){
+//   var randomValue = myArray[Math.floor(Math.random() * listOfWords.length)];
+//   res.render('index', {listOfWords: listOfWords})
+// });
 
 //function to push the letter from the input field into the guessed letters array.
 app.post('/', function (req, res){
-  data.guessedLetters.push(req.body.letterInput);
-  function validate(input){
+  guessedLetters.push(req.body.letterInput);
+  console.log(guessedLetters);
+  function validate(){
   letterInput.value = letterInput.value.replace(/\W|\d/g, '').substr(0, 1).toUpperCase();
   }
   res.redirect('/');
