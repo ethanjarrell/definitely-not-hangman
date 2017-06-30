975c9aa542c8f11d5f0bddba5182da84fa634f41
[Ethan Jarrell] <[ethan.jarrell@gmail.com]>
2017-06-30 08:31:18 -0400

    my last commit

--- app/data.js
+++ app/data.js
@@ -1,34 +1,32 @@
 const fs = require('fs');
 const listOfWords = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+let guessedLetters = [];
 
-
+// choose a random word out of an array of words
   function getRandomWord(myArray){
     var randomValue = myArray[Math.floor(Math.random() * myArray.length)];
     return randomValue;
     }
     var randomWord = getRandomWord(listOfWords);
 
+//Turn the word into an array of objects
     function makeWordAnObject(strArr){
     strArr.split("");
     str = [];
     for (var i = 0; i < strArr.length; i++){
     let c = strArr[i];
-    str.push({"letter": c})};
+    str.push({"letter": c, "value":"_",})};
      return{str};
     }
 
    let newWord = makeWordAnObject(randomWord);
    console.log(newWord);
 
-//    var findOne = function (letterarray, wordarray) {
-//     return wordarray.some(function (v) {
-//         return letterarray.indexOf(v) >= 0;
-//     });
-// };
-//
-// findOne(guessedLetters, newWord);
-// console.log(findOne);
+
+
+
 
    module.exports = {
-     newWord: str
+     newWord: str,
+     guessedLetters: guessedLetters
    }
